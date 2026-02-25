f47d5b21223bbc410fd3eca3dd136e4889a3dbc8
TheSkyroo <ishaant69@gmail.com>
2026-02-25 15:12:48 +0530

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import React from 'react'
-
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-orange-500 font-bold'>
-        Welcome to awwards
-      </h1>
+      <h1 className="text-5xl text-orange-500 font-bold">Welcome to awwards</h1>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
