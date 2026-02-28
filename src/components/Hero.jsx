2c751d4c6fa76c9afbd7e238f5c23f96f4204167
TheSkyroo <ishaant69@gmail.com>
2026-02-28 22:31:27 +0530

    css updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -40,6 +40,7 @@ function Hero() {
                 id="current-video"
                 className="size-64 origin-center scale-150 object-cover object-center"
                 onLoadedData={handleVideoLoad}
+              
               />
             </div>
           </div>
