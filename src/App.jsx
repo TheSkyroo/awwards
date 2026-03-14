4f7c4d331828850d72d5b2d5249eb8bc028cde32
TheSkyroo <ishaant69@gmail.com>
2026-03-14 20:08:26 +0530

    batman

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 const App = () => {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden bg-zinc-600">
+    <main className="relative min-h-screen w-screen overflow-x-hidden ">
       <Navbar />
       <Hero />
       <About />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-
+import React, { useRef } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+const navItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
 const Navbar = () => {
+  const navContainerRef = useRef(null);
   return (
-    <div>Navbar</div>
-  )
-}
+    <div
+      ref={navContainerRef}
+      className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
+    >
+      <header className="absolute top-1/2 w-full -translate-y-1/2 ">
+        <nav className="flex size-full items-center justify-between p-4">
+          <div className="flex item-center gap-7">
+            <img src="public/img/logo.png" alt="logo" className="w-10" />
+            <Button
+              id="product-button"
+              title="Products"
+              rightIcon={<TiLocationArrow />}
+              containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
+            />
+          </div>
 
-export default Navbar
+          <div className="flex h-full item-center">
+            <div className="hidden md:block">
+              {navItems.map((item) => (
+                <a
+                  key={item}
+                  href={`#${item.toLowerCase()}`}
+                  className="nav-hover-btn"
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
