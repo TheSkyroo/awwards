8a69ed56497d72d1a5f0ee75dc89c3d55cbdcdc2
TheSkyroo <ishaant69@gmail.com>
2026-03-15 22:04:27 +0530

    batman

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -99,10 +99,10 @@ function Hero() {
       >
         <div>
           {/* 1. Mini Video Preview */}
-          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg hidden sm:block">
             <div
               onClick={handleMiniVdClick}
-              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100 max-sm:scale-100 max-sm:opacity-100"
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
             >
               <video
                 src={getVideoSrc(upcomingVideoIndex)}
