2ea63e284ce6ca06ab2d71299639cfa275e6e53e
TheSkyroo <ishaant69@gmail.com>
2026-03-15 20:02:14 +0530

    batman

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Features from "./components/Features";
+import Story from "./components/Story";
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden ">
@@ -9,6 +10,7 @@ const App = () => {
       <Hero />
       <About />
       <Features />
+      <Story />
     </main>
   );
 };
